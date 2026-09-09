f665e0d00a5b16f2f14484c1869b175f4a1e663a
dauzkepow <dauzcool20@gmail.com>
2026-09-09 13:34:03 +0700

    add Login API

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+// Helper class untuk format respon API JSON
+
+namespace App\Helpers; // file ini berada di folder app\Helpers
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+
+class ApiResponse
+{
+    // respon success
+    public static function success (
+        JSONResource|array|null $data = null, // data yang dikirim
+        string $message = 'Success', // pesan success
+        int $status = Response::HTTP_OK // kode http status
+    ){
+        // respon yang dikembalikan
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $status);
+    }
+
+    // error
+    public static function error (
+        string $message,
+        int $status = Response::HTTP_BAD_REQUEST,
+        array $errors = []
+    ){
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $errors,
+        ], $status);
+    }
+}
---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthenticateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // login, verifikasi email dan password lalu berikan bearer token jika data sesuai
+    public function login(AuthenticateUserRequest $request)
+    {
+        // buat variabel untuk ambil data input email dan password yang divalidasi AuthenticateUserRequest
+        $credentials = $request->validated();
+
+        // cari user berdasarkan email yang diinputkan
+        $user = User::where('email', $credentials['email'])->first();
+
+        // pengecekan email tidak ditemukan atau password tidak sesuai
+        if (!$user || !Hash::check($credentials['password'], $user->password))
+            {
+                // return pengecekan tidak valid panggil Helper ApiResponse.php
+                return ApiResponse::error(
+                    'Invalid credentials',
+                    Response::HTTP_UNAUTHORIZED
+                );
+            }
+
+            // jika user sesuai, auth_token = kunci gerbang
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return ApiResponse::success(
+                [
+                    'token' => $token,
+                    'user' => $user, // cara-1 tampilkan semua
+                ],
+                'Login Successfully'
+            );
+    }
+}
